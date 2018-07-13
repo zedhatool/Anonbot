68f869050c1a3ae2ba1dd3aeb2dc05f93ec20244
TechBug2012 <mattbstanciu@gmail.com>
2018-07-13 13:00:23 -0400

    Uploads image but with the last submission

--- server.js
+++ server.js
@@ -1,33 +1,46 @@
+var fs = require('fs');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var sleep = require('sleep');
 var Client = require('instagram-private-api').V1;
 var device = new Client.Device('bogdan.stencil');
 var storage = new Client.CookieFileStorage(__dirname + '/cookies/bogdan.json');
-//const mergeImages = require('merge-images');
+const pngToJpeg = require('png-to-jpeg');
 const { createCanvas, loadImage, registerFont } = require('canvas');
 registerFont('./SourceCodePro-Regular.ttf', {family: 'SourceCodePro'});
 const canvas = createCanvas(1080, 1080);
-const ctx = canvas.getContext('2d')
+const ctx = canvas.getContext('2d');
 
 app.use('/public', express.static('public'));
 
 function createImage(text) {
-  loadImage('./graygradient.jpg').then((image) => {
-    ctx.drawImage(image, 1080, 0, 1080, 1080);
-    ctx.font = '40px "SourceCodePro"';
-    ctx.fillText(text, 500, 20);
-  })
+  ctx.fillStyle = '#606060';
+  ctx.fillRect(0, 0, 1080, 1080);
+  ctx.font = '40px "SourceCodePro"';
+  ctx.fillStyle = '#FFF';
+  ctx.fillText(text, 100, 50);
+
+  var buf = canvas.toBuffer();
+  fs.writeFileSync("test.png", buf);
+
+  //convert to jpeg becuase api only currently support jpeg
+  let buffer = fs.readFileSync("./test.png");
+  pngToJpeg()(buffer)
+    .then(output => fs.writeFileSync("./test.jpeg", output));
 }
 
 io.on('connection', function(socket) {
   socket.on('submission', function(data) {
-    Client.Session.create(device, storage, 'bogdan.stencil', process.env.BOGDANPASSWORD)
+    console.log("received " + data);
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    createImage(data);
+    Client.Session.create(device, storage, 'bogdan.stencil', 'stancium1151')
       .then(function(session) {
-        Client.Upload.photo(session, createImage(data))
+        Client.Upload.photo(session, './test.jpeg')
           .then(function(upload) {
-            console.log(upload.params.uploadId);
+            console.log("id: " + upload.params.uploadId);
 		        return Client.Media.configurePhoto(session, upload.params.uploadId, data);
           })
           .then(function(medium) {
