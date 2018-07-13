bc0c52bdfc75812dc9c9d7b5dbc11e08d0686ac0
TechBug2012 <mattbstanciu@gmail.com>
2018-07-13 17:55:29 -0400

    publishing now works properly

--- server.js
+++ server.js
@@ -33,32 +33,49 @@ function createImage(text) {
   fs.unlinkSync('./test.png');
 }
 
-function resetStorage() {
-  fs.unlinkSync('./cookies/bogdan.json');
-  fs.writeFileSync("./cookies/bogdan.json", "");
+function getData() {
+  return fs.readFileSync('./submission.txt', 'utf-8');
+}
+function clearData() {
+  fs.truncate('./submission.txt', 0, function(){console.log('submission file cleared')});
 }
 
 io.on('connection', function(socket) {
   socket.on('submission', function(data) {
     console.log("received " + data);
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    //fs.unlinkSync('./test.jpeg');
-    resetStorage();
     createImage(data);
+    fs.writeFile("./submission.txt", data, 'utf-8', function(err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("submission saved");
+    });
+  });
+});
 
+function publish() {
+  var contents = fs.readFileSync('./submission.txt', 'utf-8');
+  if (contents.length != 0) {
+    var data = getData();
+    console.log("Data: " + data);
     Client.Session.create(device, storage, 'bogdan.stencil', process.env.BOGDAN_PASSWORD)
       .then(function(session) {
         Client.Upload.photo(session, './test.jpeg')
-	       .then(function(upload) {
-		         console.log(upload.params.uploadId);
-		         return Client.Media.configurePhoto(session, upload.params.uploadId, data);
-	       })
-	       .then(function(medium) {
-		          console.log(medium.params)
-	       })
+          .then(function(upload) {
+              console.log(upload.params.uploadId);
+              return Client.Media.configurePhoto(session, upload.params.uploadId, data);
+          })
+          .then(function(medium) {
+              console.log(medium.params)
+          })
       });
-  });
-});
+
+      clearData();
+  }
+} setInterval(publish, 5000);
+
+publish();
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/index.html');
