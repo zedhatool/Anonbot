3a0d47b2de2864caa099862315538218de5e88a3
MatthewStanciu <mattbstanciu@gmail.com>
2018-08-12 22:12:16 -0400

    reorganized code
    put all the routing stuff at the bottom and all of the necessary functions at the top

--- server.js
+++ server.js
@@ -15,9 +15,83 @@ registerFont('./fonts/SourceCodePro-Regular.ttf', {family: 'SourceCodePro'});
 const canvas = createCanvas(1080, 1080);
 const ctx = canvas.getContext('2d');
 
-var ref, urlSegmentToInstagramId;
-ref = require('instagram-id-to-url-segment');
-urlSegmentToInstagramId = ref.urlSegmentToInstagramId;
+function createImage(text, fillStyle) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  var formatted = wrap(text, {indent: '', width: 28});
+  ctx.fillStyle = fillStyle;
+  ctx.fillRect(0, 0, 1080, 1080);
+  ctx.font = '62px "SourceCodePro"';
+  ctx.fillStyle = '#FFF';
+  ctx.fillText(formatted, 17, 65);
+
+  var buf = canvas.toBuffer();
+  fs.writeFileSync("submission.png", buf);
+
+  //convert to jpeg becuase currently api only supports jpeg
+  let buffer = fs.readFileSync("./submission.png");
+  pngToJpeg()(buffer)
+    .then(output => fs.writeFile("./submission.jpeg", output, function(err) {
+      if (err) console.log(err);
+      fs.exists("./submission.jpeg", function(exists) {
+        if (exists) publish(text);
+      })
+    }));
+  fs.unlinkSync('./submission.png');
+}
+
+function publish(caption) {
+  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
+  .then(function(session) {
+    Client.Upload.photo(session, './submission.jpeg')
+    .then(function(upload) {
+      console.log("uploading...");
+      return Client.Media.configurePhoto(session, upload.params.uploadId, caption);
+    })
+    .then(function(medium) {
+      console.log("photo uploaded at " + medium.params.webLink);
+    })
+  });
+  log("post", caption);
+}
+
+function postComment(id, comment) {
+  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
+  .then(function(session) {
+    return Client.Comment.create(session, ''+id, comment);
+  })
+}
+
+function delPost(id) {
+  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
+  .then(function(session) {
+    return Client.Media.delete(session, ''+id);
+  })
+}
+
+function log(type, data) {
+  var date = new Date();
+
+  fs.readFile('./logs-'+type+'.json', 'utf-8', function(err, result) {
+    if (err) console.log(err);
+    else {
+      var obj = JSON.parse(result);
+      if (type === "submission") obj.submissionMade.push({hour: date, addr: data});
+      else if (type === "post") obj.postMade.push({hour: date, post: data});
+
+      var json = JSON.stringify(obj);
+      fs.writeFile('./logs-'+type+'.json', json, 'utf-8', function(err) {
+        if (err) console.log(err);
+      })
+    }
+  })
+}
+
+function getClientIP(req){ // Anonbot logs IPs for safety & moderation
+    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+}
+
+var ref = require('instagram-id-to-url-segment');
+var urlSegmentToInstagramId = ref.urlSegmentToInstagramId;
 
 function getShortcode(url) {
   var parts = url.split('/');
@@ -79,80 +153,6 @@ app.post("/modpost", function(req, res) {
     return res.redirect('/');
   }
 })
-
-function createImage(text, fillStyle) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  var formatted = wrap(text, {indent: '', width: 28});
-  ctx.fillStyle = fillStyle;
-  ctx.fillRect(0, 0, 1080, 1080);
-  ctx.font = '62px "SourceCodePro"';
-  ctx.fillStyle = '#FFF';
-  ctx.fillText(formatted, 17, 65);
-
-  var buf = canvas.toBuffer();
-  fs.writeFileSync("submission.png", buf);
-
-  //convert to jpeg becuase currently api only supports jpeg
-  let buffer = fs.readFileSync("./submission.png");
-  pngToJpeg()(buffer)
-    .then(output => fs.writeFile("./submission.jpeg", output, function(err) {
-      if (err) console.log(err);
-      fs.exists("./submission.jpeg", function(exists) {
-        if (exists) publish(text);
-      })
-    }));
-  fs.unlinkSync('./submission.png');
-}
-
-function publish(caption) {
-  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
-  .then(function(session) {
-    Client.Upload.photo(session, './submission.jpeg')
-    .then(function(upload) {
-      console.log("uploading...");
-      return Client.Media.configurePhoto(session, upload.params.uploadId, caption);
-    })
-    .then(function(medium) {
-      console.log("photo uploaded at " + medium.params.webLink);
-    })
-  });
-  log("post", caption);
-}
-function postComment(id, comment) {
-  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
-  .then(function(session) {
-    return Client.Comment.create(session, ''+id, comment);
-  })
-}
-
-function delPost(id) {
-  Client.Session.create(device, storage, 'anonbot.wl', process.env.ANON_PASSWORD)
-  .then(function(session) {
-    return Client.Media.delete(session, ''+id);
-  })
-}
-
-function log(type, data) {
-  var date = new Date();
-
-  fs.readFile('./logs-'+type+'.json', 'utf-8', function(err, result) {
-    if (err) console.log(err);
-    else {
-      var obj = JSON.parse(result);
-      if (type === "submission") obj.submissionMade.push({hour: date, addr: data});
-      else if (type === "post") obj.postMade.push({hour: date, post: data});
-
-      var json = JSON.stringify(obj);
-      fs.writeFile('./logs-'+type+'.json', json, 'utf-8', function(err) {
-        if (err) console.log(err);
-      })
-    }
-  })
-}
-
-function getClientIP(req){ // Anonbot logs IPs for safety & moderation
-    return req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-}
 
 app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
